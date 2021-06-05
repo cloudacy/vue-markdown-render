7339359fc32eec553ccd1f9f34fc3e5058cb454b
Patrick Kuen <patrick.kuen11@gmail.com>
2021-06-05 12:24:06 +0200

    chore: format example file

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,44 +1,44 @@
-import Vue, { VNode } from "vue";
-import VueMarkdown, {Options} from "../../dist/VueMarkdown";
+import Vue, { VNode } from 'vue'
+import VueMarkdown, { Options } from '../../dist/VueMarkdown'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 const App = Vue.extend({
-  name: "App",
+  name: 'App',
   components: {
-    VueMarkdown
+    VueMarkdown,
   },
   data() {
     return {
       i: 0,
       options: {
-        html: true
-      } as Options
-    };
+        html: true,
+      } as Options,
+    }
   },
   render(h): VNode {
-    return h("div", [
+    return h('div', [
       h(
-        "button",
+        'button',
         {
           on: {
             click: () => {
-              this.i++;
-            }
-          }
+              this.i++
+            },
+          },
         },
-        "Increment"
+        'Increment'
       ),
-      h("vue-markdown", {
+      h('vue-markdown', {
         props: {
-          source: `# This is a markdown heading\n## This is your number: ${this.i}. <i>HTML is allowed via options</i>`,
-          options: this.options
-        }
-      })
-    ]);
-  }
-});
+          source: `# This is a markdown heading\n## This is your number: ${this.i}.\n<i>HTML is allowed via options</i>`,
+          options: this.options,
+        },
+      }),
+    ])
+  },
+})
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app')
