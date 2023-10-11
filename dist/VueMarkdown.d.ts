79034f35dfa66e32ff6b5ae1aa85910ca036c0b7
Maciej Stępyra <jakudlaty@users.noreply.github.com>
2023-10-11 13:27:35 +0200

    Dist file change

--- dist/VueMarkdown.d.ts
+++ dist/VueMarkdown.d.ts
@@ -1,4 +1,4 @@
 import { Component } from 'vue';
-export { Options } from 'markdown-it';
+export type { Options } from 'markdown-it';
 declare const VueMarkdown: Component;
 export default VueMarkdown;
