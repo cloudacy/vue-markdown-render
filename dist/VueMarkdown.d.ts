f5eb555008bc3e419b3da558274b9c4b0cf00fb9
Patrick <p.kuen@cloudacy.com>
2024-05-24 06:59:16 +0200

    refactor: use more modern style to define component

--- dist/VueMarkdown.d.ts
+++ dist/VueMarkdown.d.ts
@@ -1,4 +1,12 @@
-import { type Component } from "vue";
+import MarkdownIt from "markdown-it";
 export type { Options } from "markdown-it";
-declare const VueMarkdown: Component;
+declare const VueMarkdown: import("vue").DefineSetupFnComponent<{
+    source: string;
+    options?: MarkdownIt.Options | undefined;
+    plugins?: MarkdownIt.PluginSimple[] | undefined;
+}, {}, {}, {
+    source: string;
+    options?: MarkdownIt.Options | undefined;
+    plugins?: MarkdownIt.PluginSimple[] | undefined;
+} & {}, import("vue").PublicProps>;
 export default VueMarkdown;
--- src/VueMarkdown.ts
+++ src/VueMarkdown.ts
@@ -2,34 +2,16 @@ import MarkdownIt, {
   type Options as MarkdownItOptions,
   type PluginSimple,
 } from "markdown-it";
-import {
-  type Component,
-  type PropType,
-  computed,
-  defineComponent,
-  h,
-  ref,
-} from "vue";
+import { computed, defineComponent, h, ref } from "vue";
 export type { Options } from "markdown-it";
 
-const VueMarkdown: Component = defineComponent({
-  name: "VueMarkdown",
-  props: {
-    source: {
-      type: String,
-      required: true,
-    },
-    options: {
-      type: Object as PropType<MarkdownItOptions>,
-      required: false,
-    },
-    plugins: {
-      type: Array as PropType<PluginSimple[]>,
-      required: false,
-    },
-  },
-  setup(props) {
-    const md = ref<MarkdownIt>(new MarkdownIt(props.options ?? {}));
+const VueMarkdown = defineComponent(
+  (props: {
+    source: string;
+    options?: MarkdownItOptions;
+    plugins?: PluginSimple[];
+  }) => {
+    const md = ref(new MarkdownIt(props.options ?? {}));
 
     for (const plugin of props.plugins ?? []) {
       md.value.use(plugin);
@@ -39,6 +21,9 @@ const VueMarkdown: Component = defineComponent({
 
     return () => h("div", { innerHTML: content.value });
   },
-});
+  {
+    props: ["source", "options", "plugins"],
+  }
+);
 
 export default VueMarkdown;
