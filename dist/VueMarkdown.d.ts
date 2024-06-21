8f82e775c075e9d19c6179ca5d0fbc6acdef80c0
Patrick <p.kuen@cloudacy.com>
2024-06-21 06:41:02 +0200

    chore: upgrade to markdown-it v14 closes #31

--- dist/VueMarkdown.d.ts
+++ dist/VueMarkdown.d.ts
@@ -1,12 +1,12 @@
-import MarkdownIt from "markdown-it";
+import { type Options as MarkdownItOptions, type PluginSimple } from "markdown-it";
 export type { Options } from "markdown-it";
 declare const VueMarkdown: import("vue").DefineSetupFnComponent<{
     source: string;
-    options?: MarkdownIt.Options | undefined;
-    plugins?: MarkdownIt.PluginSimple[] | undefined;
+    options?: MarkdownItOptions | undefined;
+    plugins?: PluginSimple[] | undefined;
 }, {}, {}, {
     source: string;
-    options?: MarkdownIt.Options | undefined;
-    plugins?: MarkdownIt.PluginSimple[] | undefined;
+    options?: MarkdownItOptions | undefined;
+    plugins?: PluginSimple[] | undefined;
 } & {}, import("vue").PublicProps>;
 export default VueMarkdown;
