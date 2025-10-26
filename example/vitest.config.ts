789ceb119acc4fd00f19093704a7f58451c835d1
Patrick <p.kuen@cloudacy.com>
2025-10-26 07:20:34 +0100

    refactor: format new files

--- example/vitest.config.ts
+++ example/vitest.config.ts
@@ -1,17 +1,17 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
-	test: {
-		globals: true,
-		environment: 'jsdom',
-	},
-})
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
